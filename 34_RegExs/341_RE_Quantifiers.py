825c69f517d2fbfafff07c8978b821fcb2d665e9
Antun <antun.jerkovic@google.com>
2022-07-20 15:41:15 +0200

    re2

--- 34_RegExs/341_RE_Quantifiers.py
+++ 34_RegExs/341_RE_Quantifiers.py
@@ -9,5 +9,13 @@
 ?	    Once or none(optional)
 
 # k     svi k
-# k\w   like kittens kitty
-# k\w+  like kittens kitty
+# k\w   like kittens kitty (ke ki ki)
+# k\w+  like kittens kitty (ke kittens kitty)
+# \sk\w+    _kittens _kitty
+# \w\w\w\w\w = \w{5} hello world aaabbbccc (aaabb)
+# \w{9} aaabbbccc
+# \d{3} \d{3}-\d{4} = 415 412-9876
+# ab*c = aaabbbccc (abbbc) = aaabccc (abc) = aaaccc (ac)
+# ab*c*d = aaabbbcccdd (abbbcccd)
+# kitens = kitens; kitens? = kiten = kitens
+# \d{3} \d{3}-?\d{4} = 415 412-9876 = 415 4129876

05d679f4800ab4e054700441ae9c3cb1a112ec87
Antun <antun.jerkovic@gmail.com>
2022-07-24 06:40:11 +0200

    MalaNadopuna

---
+++ 37_DodaciJA/00_Linkovi.py
@@ -0,0 +1,19 @@
+
+# todo VirtualEnv
+# https://pdm.fming.dev/latest/
+# https://testdriven.io/blog/python-environments/
+# https://testdriven.io/guides/complete-python/
+# https://realpython.com/python-virtual-environments-a-primer/
+# https://dev.to/bowmanjd/python-tools-for-managing-virtual-environments-3bko
+# https://www.laac.dev/blog/setting-up-modern-python-development-environment-ubuntu-20/
+# https://medium.com/ntust-aivc/how-to-install-pyenv-pipenv-in-ubuntu-and-use-multiple-versions-of-python-and-its-suites-3514099a6e05
+# https://getbootstrap.com/docs/5.1/getting-started/introduction/
+# https://towardsdatascience.com/comparing-python-virtual-environment-tools-9a6543643a44
+
+# todo DevelopmentEnv
+# https://roadmap.sh/roadmaps
+# https://www.youtube.com/theroadmap?sub_confirmation=1
+# https://www.youtube.com/channel/UC1qkMXH8d2I9DDAtBSeEHqg/videos
+# https://www.geeksforgeeks.org/python-web-development-django-tutorial/
+# https://developer.mozilla.org/en-US/docs/Web/JavaScript
+# https://www.pythonforbeginners.com/basics/

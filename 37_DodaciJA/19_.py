3493e7a8f63cda513364de18d44494184f6bfbb1
Antun <antun.jerkovic@gmail.com>
2022-08-05 20:01:05 +0200

    dk

---
+++ 37_DodaciJA/19_.py
@@ -0,0 +1,122 @@
+# Perfect Your Python Development Setup
+# Learning Path ⋅ Skills: Python Setup, Code Editors & IDEs, Running Apps
+#
+# Python IDEs and Code Editors: The Complete Guide
+# Optimize your Python development setup with this learning path covering installing Python 3 correctly, choosing the right code editor for # you, as well as tips for working with virtual environments and testing your Python apps.
+#
+# Perfect Your Python Development Setup
+# Learning Path ⋅ 12 Resources
+#
+# Introduction to Python 3
+# Course
+#
+# Installing Python on Windows, macOS, and Linux
+# To get started working with Python, you'll need to have access to the Python interpreter. There are several common ways to accomplish # this and in this course, you will learn how to install the latest version of Python on your computer.
+#
+# Python IDEs and Code Editors: The Complete Guide
+# Course
+#
+# Finding the Perfect Python Code Editor
+# Find your perfect Python development setup with this review of Python IDEs and code editors. Writing Python using IDLE or the Python REPL # is great for simple things, but not ideal for larger programming projects. With this course you'll get an overview of the most common # Python coding environments to help you make an informed decision.
+#
+# Python Development in Visual Studio Code
+# Course
+#
+# Setup Guide: Visual Studio Code + Python
+# ⠀
+#
+# Thonny: The Beginner-Friendly Python Editor
+# Course
+#
+# Setup Guide: Thonny, the Beginner-Friendly Python IDE
+# ⠀
+#
+# Jupyter Notebook: An Introduction
+# Course
+#
+# Setup Guide: Jupyter Notebooks + Python
+# ⠀
+#
+# Setting Up Sublime Text 3 for Python Developers
+# Tutorial
+#
+# Setup Guide: Sublime Text + Python
+# ⠀
+#
+# PyCharm for Productive Python Development (Guide)
+# Tutorial
+#
+# Setup Guide: PyCharm + Python
+# ⠀
+#
+# Emacs: The Best Python Editor?
+# Tutorial
+#
+# Setup Guide: Emacs + Python
+# ⠀
+#
+# Python and VIM
+# Tutorial
+#
+# Setup Guide: VIM + Python
+# ⠀
+#
+# An Effective Python Environment: Making Yourself at Home
+# Tutorial
+#
+# An Effective Python Environment: Making Yourself at Home
+# This guide will walk you through the decisions you need to make when customizing your development environment for working with Python.
+#
+# How to Run Your Python Scripts
+# Course
+#
+# Running Python Scripts
+# This step-by-step course will guide you through a series of ways to run Python scripts, depending on your environment, platform, needs, # and skills as a programmer.
+#
+# What Is pip? A Guide for New Pythonistas
+# Course
+#
+# A Beginner's Guide to Pip
+# What is pip? In this beginner-friendly course, you'll learn how to use pip, the standard package manager for Python, so that you can # install and manage additional packages that are not part of the Python standard library.
+#
+# Python Virtual Environments (venv)
+# Course
+#
+# Working With Python Virtual Environments
+# This course demonstrates how Python's virtual environments work as a "sandbox" and you get a quick walkthrough on how to set up a new # environment (or virtualenv, as they're called for short) and how to install third-party packages into it using the pip command.
+#
+# Managing Multiple Python Versions With pyenv
+# Course
+#
+# Start Managing Multiple Python Versions With pyenv
+# Learn how to install multiple Python versions and switch between them with ease, including project-specific virtual environments, with # pyenv.
+#
+# A Guide to Pipenv, the new Python Packaging Tool
+# Course
+#
+# Working With Pipenv
+# Pipenv is a packaging tool for Python that solves some common problems associated with the typical workflow using pip, virtualenv, and # requirements.txt. In this course, you'll go over what problems Pipenv solves and how to manage your Python dependencies with it.
+#
+# Getting Started With Testing in Python
+# Course
+#
+# Running Python Tests With PyTest
+# In this hands-on course, you’ll see how to create Python unit tests, execute them, and find the bugs before your users do. You’ll learn # about the tools available to write and execute tests, check your application’s performance, and even look for security issues.
+#
+# Git Tips for Python Developers
+# Course
+#
+# Introduction to Git and GitHub for Python Developers
+# What is Git, what is GitHub, and what's the difference? Learn the basics of Git and GitHub from the perspective of a Pythonista in this # step-by-step video course.
+#
+# Run Python Versions in Docker: How to Try the Latest Python Release
+# Tutorial
+#
+# Run Python Versions in Docker: How to Try the Latest Python Release
+# Learn how to run different Python versions in Docker. By following the examples, you'll see how you can play with the latest development # version of Python, and how to use Dockerfiles to set up Python environments and package your own scripts.
+#
+# Managing Python Dependencies Course
+# Course
+#
+# Managing Python Dependencies
+# Get up to speed with Python dependency management quickly and go from “writing scripts” to “building applications” with this complete # course.
---
+++ 37_DodaciJA/20_.py
@@ -0,0 +1,561 @@
+# Python Web Development Tutorials
+# Python is a beautiful language. It’s easy to learn and fun, and its syntax (the rules) is clear and concise. Python is a popular choice # for beginners, yet still powerful enough to back some of the world’s most popular products and applications from companies like NASA, # Google, IBM, Cisco, Microsoft, Industrial Light & Magic among others.
+#
+# One area where Python shines is web development. Python offers many frameworks from which to choose from including bottle.py, Flask, # CherryPy, Pyramid, Django and web2py. These frameworks have been used to power some of the world’s most popular sites such as Spotify, # Mozilla, Reddit, the Washington Post and Yelp. The tutorials and articles in this section cover techniques used in the development of # Python Web applications and focus on how to program real-world solutions to problems that ordinary people actually want to solve.
+#
+# What you will learn from this section
+# Testing, mocking and Test Driven Development
+# Integrating back-end code with front-end libraries and frameworks
+# How to deploy your Python Web Application
+# Containerizing web applications
+# Designing and interacting with APIs
+# Social Authentication
+# Improving your application’s performance through caching
+# Free Bonus: Click here to get access to a free Django Learning Resources Guide (PDF) that shows you tips and tricks as well as common # pitfalls to avoid when building Python + Django web applications.
+#
+# Free Bonus: Click here to get access to a free Flask + Python video tutorial that shows you how to build Flask web app, step-by-step.
+#
+# Search…
+# Build a Flashcards App With Django
+# Build a Flashcards App With Django
+# Jul 18, 2022  django intermediate projects web-dev
+#
+# Data Management With Python, SQLite, and SQLAlchemy
+# SQLite and SQLAlchemy in Python: Moving Your Data Beyond Flat Files
+# Jul 12, 2022  databases intermediate web-dev
+#
+# A First Look at PyScript: Python in the Web Browser
+# A First Look at PyScript: Python in the Web Browser
+# Jun 06, 2022  data-science front-end gui intermediate web-dev
+#
+# Build a URL Shortener With FastAPI and Python
+# Build a URL Shortener With FastAPI and Python
+# May 18, 2022  api databases intermediate projects web-dev
+#
+# Deploying a Python Flask Example Application Using Heroku
+# Deploying a Flask Application Using Heroku
+# May 10, 2022  devops flask intermediate web-dev
+#
+# Use FastAPI to Build Web APIs
+# Python REST APIs With FastAPI
+# Apr 05, 2022  api basics web-dev
+#
+# Pagination for a User-Friendly Django App
+# Pagination for a User-Friendly Django App
+# Mar 28, 2022  django intermediate web-dev
+#
+# Manage Your To-Do Lists Using Python and Django
+# Manage Your To-Do Lists Using Python and Django
+# Mar 02, 2022  basics django projects web-dev
+#
+# Python's urllib.request for HTTP Requests
+# Python's urllib.request for HTTP Requests
+# Feb 28, 2022  api intermediate web-dev web-scraping
+#
+# Python Socket Programming
+# Socket Programming in Python (Guide)
+# Feb 21, 2022  advanced python web-dev
+#
+# Build and Submit HTML Forms With Django – Part 4
+# Build and Submit HTML Forms With Django – Part 4
+# Jan 12, 2022  django intermediate web-dev
+#
+# Build and Handle POST Requests in Django – Part 3
+# Build and Handle POST Requests in Django – Part 3
+# Jan 10, 2022  django intermediate web-dev
+#
+# Build a Django Front End With Bulma - Part 2
+# Build a Django Front End With Bulma – Part 2
+# Jan 05, 2022  django intermediate web-dev
+#
+# Python Web Applications: Deploy Your Script as a Flask App
+# Deploy Your Python Script on the Web With Flask
+# Jan 04, 2022  devops flask intermediate web-dev
+#
+# Build a Social Network With Django - Part 1
+# Build a Social Network With Django – Part 1
+# Jan 03, 2022  django intermediate web-dev
+#
+# Hosting a Django Project on Heroku
+# Host Your Django Project on Heroku
+# Dec 14, 2021  devops django intermediate python web-dev
+#
+# Custom Tags and Filters in Django Templates
+# Django Templates: Implementing Custom Tags and Filters
+# Oct 27, 2021  advanced django web-dev
+#
+# Hosting a Django Project on Heroku
+# Hosting a Django Project on Heroku
+# Sep 29, 2021  django intermediate projects web-dev
+#
+# Build a Personal Diary With Django and Python
+# Build a Personal Diary With Django and Python
+# Sep 15, 2021  basics django front-end projects web-dev
+#
+# Python API Integration
+# Python and REST APIs: Interacting With Web Services
+# Jul 28, 2021  api intermediate web-dev
+#
+# Django Setup Guide
+# Your First Steps With Django: Set Up a Django Project
+#  basics best-practices django web-dev
+#
+# Use FastAPI to Build Web APIs
+# Using FastAPI to Build Python Web APIs
+#  api basics web-dev
+#
+# Django Setup Guide
+# How to Set Up a Django Project
+#  basics best-practices django web-dev
+#
+# Deploying a Python Flask Example Application Using Heroku
+# Deploying a Python Flask Example Application Using Heroku
+#  devops flask intermediate web-dev
+#
+# Get Started With Django Part 3: Django View Authorization
+# Django View Authorization: Restricting Access
+#  django intermediate web-dev
+#
+# Brython: Python in Your Browser
+# Brython: Python in Your Browser
+#  front-end intermediate web-dev
+#
+# Python Web Applications: Deploy Your Script as a Flask App
+# Python Web Applications: Deploy Your Script as a Flask App
+#  devops flask intermediate web-dev
+#
+# Python Django Tutorials Artwork
+# Building HTTP APIs With Django REST Framework
+#  advanced api django web-dev
+#
+# Customize the Django Admin With Python
+# Django Admin Customization
+#  advanced django web-dev
+#
+# Get Started With Django Part 3: Django View Authorization
+# Get Started With Django Part 3: Django View Authorization
+#  django intermediate web-dev
+#
+# Data Management With Python, SQLite, and SQLAlchemy
+# Data Management With Python, SQLite, and SQLAlchemy
+#  databases intermediate web-dev
+#
+# Create a Flask Application With Google Login
+# Using Google Login With Flask
+#  flask intermediate web-dev
+#
+# Exploring HTTPS With Python
+# Exploring HTTPS and Cryptography in Python
+#  intermediate web-dev
+#
+# Django Redirects Tutorial
+# Django Redirects
+#  django intermediate web-dev
+#
+# Customize the Django Admin With Python
+# Customize the Django Admin With Python
+#  advanced django web-dev
+#
+# 13 Project Ideas for Intermediate Python Developers
+# Grow Your Python Portfolio With 13 Intermediate Project Ideas
+#  gui intermediate projects web-dev
+#
+# How to Write an Installable Django App
+# How to Write an Installable Django App
+#  advanced django projects testing web-dev
+#
+# How to Move a Django Model to Another App
+# How to Move a Django Model to Another App
+#  advanced django web-dev
+#
+# Use a Flask Blueprint to Architect Your Applications
+# Use a Flask Blueprint to Architect Your Applications
+#  flask intermediate web-dev
+#
+# Exploring HTTPS With Python
+# Exploring HTTPS With Python
+#  intermediate web-dev
+#
+# Building Flask REST APIs
+# Python REST APIs With Flask, Connexion, and SQLAlchemy – Part 4
+#  flask front-end intermediate web-dev
+#
+# Get Started With Django Part 1: Build a Portfolio App
+# Get Started With Django: Build a Portfolio App
+#  basics django projects web-dev
+#
+# What You Need to Know to Manage Users in Django Admin
+# What You Need to Know to Manage Users in Django Admin
+#  advanced django web-dev
+#
+# Django Migrations: A Primer
+# Digging Deeper Into Django Migrations
+#  databases django intermediate web-dev
+#
+# Create a Flask Application With Google Login
+# Create a Flask Application With Google Login
+#  flask intermediate python web-dev
+#
+# Make a Location-Based Web App With Django and GeoDjango
+# Make a Location-Based Web App With Django and GeoDjango
+#  django intermediate web-dev
+#
+# Sending Emails With Python
+# Sending Emails With Python
+#  intermediate web-dev
+#
+# Building Flask REST APIs
+# Python REST APIs With Flask, Connexion, and SQLAlchemy – Part 3
+
+
+Get Started With Django Part 1: Build a Portfolio App
+Get Started With Django Part 1: Build a Portfolio App
+django intermediate projects web-dev
+
+Django Migrations: A Primer
+Django Migrations 101
+intermediate web-dev
+
+13 Project Ideas for Intermediate Python Developers
+13 Project Ideas for Intermediate Python Developers
+intermediate projects web-dev
+
+Python’s Requests Library(Guide)
+Making HTTP Requests With Python
+intermediate web-dev
+
+Python’s Requests Library(Guide)
+Python’s Requests Library(Guide)
+intermediate web-dev
+
+Django Migrations: A Primer
+Django Migrations: A Primer
+basics databases django web-dev
+
+Modeling Polymorphism in Django With Python
+Modeling Polymorphism in Django With Python
+advanced django web-dev
+
+Make a Location-Based Web App With Django and GeoDjango
+Make a Location-Based Web App With Django and GeoDjango
+django intermediate web-dev
+
+Sending Emails With Python
+Sending Emails With Python
+intermediate web-dev
+
+Building Serverless Apps Using AWS Chalice
+Building Serverless Python Apps Using AWS Chalice
+intermediate web-dev
+
+Building Flask REST APIs
+Python REST APIs With Flask, Connexion, and SQLAlchemy – Part 2
+api databases flask intermediate web-dev
+
+Django Redirects Tutorial
+The Ultimate Guide to Django Redirects
+django intermediate web-dev
+
+Building Flask REST APIs
+Python REST APIs With Flask, Connexion, and SQLAlchemy
+api flask intermediate web-dev
+
+Python Project for Beginners: Bitcoin Price Notifications
+Python Project for Beginners: Bitcoin Price Notifications
+basics projects web-dev
+
+Python + Memcached: Efficient Caching in Distributed Applications
+Python + Memcached: Efficient Caching in Distributed Applications
+databases intermediate web-dev
+
+Building a Simple Web App With Bottle, SQLAlchemy, and the Twitter API
+Building a Simple Web App With Bottle, SQLAlchemy, and the Twitter API
+api databases projects web-dev
+
+Code Evaluation With AWS Lambda and API Gateway
+Code Evaluation With AWS Lambda and API Gateway
+advanced devops web-dev
+
+Angular, Flask logos
+User Authentication With Angular 4 and Flask
+advanced flask front-end web-dev
+
+Test Driven Development of a Django RESTful API
+Test Driven Development of a Django RESTful API
+advanced api django testing web-dev
+
+Django channels logo
+Getting Started with Django Channels
+advanced databases django web-dev
+
+Token-Based Authentication With Flask
+Token-Based Authentication With Flask
+advanced flask web-dev
+
+Automating Django Deployments with Fabric and Ansible
+Automating Django Deployments with Fabric and Ansible
+advanced devops django web-dev
+
+Caching in Django With Redis
+Caching in Django With Redis
+databases django intermediate web-dev
+
+Flask by Example – Custom Angular Directive with D3
+Flask by Example – Custom Angular Directive with D3
+flask front-end web-dev
+
+Mocking in Python part two
+Testing External APIs With Mock Servers
+advanced api testing web-dev
+
+Mocking in Python part one
+Mocking External APIs in Python
+advanced api testing web-dev
+
+Getting Started With the Slack API Using Python and Flask
+Getting Started With the Slack API Using Python and Flask
+api flask intermediate web-dev
+
+Deploying Django + Python 3 + PostgreSQL to AWS Elastic Beanstalk
+Deploying Django + Python 3 + PostgreSQL to AWS Elastic Beanstalk
+advanced databases devops django web-dev
+
+Development and Deployment of Django on Fedora
+Development and Deployment of Django on Fedora
+devops django tools web-dev
+
+Development and Deployment of Cookiecutter-Django via Docker
+Development and Deployment of Cookiecutter-Django via Docker
+devops django docker web-dev
+
+Django, Linkedin logos
+LinkedIn Social Authentication in Django
+advanced django web-dev
+
+Image of Flask and Martini logos and Sinatra hat
+Python, Ruby, and Golang: A Web Service Application Comparison
+databases flask web-dev
+
+Asynchronous Tasks With Django and Celery
+Asynchronous Tasks With Django and Celery
+advanced databases django web-dev
+
+Create a REST API in Minutes With Pyramid and Ramses
+Create a REST API in Minutes With Pyramid and Ramses
+api web-dev
+
+Handling User Authentication With Angular and Flask
+Handling User Authentication With Angular and Flask
+flask front-end web-dev
+
+'Minimum viable test'
+The Minimum Viable Test Suite
+flask intermediate testing web-dev
+
+Django Development with Docker Compose and Machine
+Django Development with Docker Compose and Machine
+advanced devops django docker web-dev
+
+Django and postgres logos
+Fun with Django's New Postgres Features
+advanced databases django web-dev
+
+Scaffold a Flask Project
+Scaffold a Flask Project
+basics flask web-dev
+
+Flask and Docker images
+Dockerizing Flask With Compose and Machine – From Localhost to the Cloud
+advanced devops docker flask web-dev
+
+Main logo
+The Ultimate Flask Front-End – Part 2
+advanced flask front-end web-dev
+
+The Ultimate Flask Front-End
+The Ultimate Flask Front-End
+advanced flask front-end web-dev
+
+Flask by Example – Updating the UI
+Flask by Example – Updating the UI
+basics flask front-end web-dev
+
+Python Docker logo
+Docker in Action – Fitter, Happier, More Productive
+devops docker flask web-dev
+
+Deploying a Django app to AWS Elastic Beanstalk
+Deploying a Django App to AWS Elastic Beanstalk
+advanced databases devops django web-dev
+
+Optical character recognition demo results
+Setting up a Simple OCR Server
+api data-science flask intermediate web-dev
+
+Flask email confirmation
+Handling Email Confirmation During Registration in Flask
+databases flask intermediate web-dev
+
+Legos
+Model-View-Controller(MVC) Explained – With Legos
+
+
+Requests cache
+Caching External API Requests
+advanced api flask web-dev
+
+Deploying Django on Dokku
+Deploying Django on Dokku
+advanced devops django docker web-dev
+
+Automatically Scale Heroku Dynos
+Automatically Scale Heroku Dynos
+advanced devops web-dev
+
+Flask by Example – Updating the Staging Environment
+Flask by Example – Updating the Staging Environment
+devops flask web-dev
+
+Twitter Sentiment Analysis – Overall bar chart
+Twitter Sentiment Analysis – Python, Docker, Elasticsearch, Kibana
+advanced api data-science docker web-dev
+
+Adding Social Authentication to Django
+Adding Social Authentication to Django
+django web-dev
+
+Kickstarting Flask on Ubuntu – Setup and Deployment
+Kickstarting Flask on Ubuntu – Setup and Deployment
+devops flask web-dev
+
+Django Rest Framework CBVs
+Django Rest Framework – Class Based Views
+api django web-dev
+
+Flask by Example – Integrating Flask and Angular
+Flask by Example – Integrating Flask and Angular
+flask front-end web-dev
+
+Flask stock visualizer app screenshot
+Full Stack Development – Fetching Data, Visualizing with D3, and Deploying with Dokku
+data-science devops flask front-end web-dev
+
+Django Rest Framework – An Introduction
+Django Rest Framework – An Introduction
+api django web-dev
+
+Flask by Example – Implementing a Redis Task Queue
+Flask by Example – Implementing a Redis Task Queue
+databases flask web-dev
+
+Django and AJAX Form Submissions – More Practice
+Django and AJAX Form Submissions – More Practice
+django front-end web-dev
+
+Django and AJAX Form Submissions – Say 'Goodbye' to the Page Refresh
+Django and AJAX Form Submissions – Say 'Goodbye' to the Page Refresh
+django front-end web-dev
+
+Flask by Example – Text Processing with Requests, BeautifulSoup, and NLTK
+Flask by Example – Text Processing with Requests, BeautifulSoup, and NLTK
+data-science flask web-dev
+
+Django migrations
+Data Migrations
+databases django web-dev
+
+Discover Flask, Part 2 – Creating a Login Page
+Discover Flask, Part 2 – Creating a Login Page
+basics flask web-dev
+
+Discover Flask, Part 1 – Setting Up a Static Site
+Discover Flask, Part 1 – Setting Up a Static Site
+basics flask web-dev
+
+Flask by Example – Setting up Postgres, SQLAlchemy, and Alembic
+Flask by Example – Setting up Postgres, SQLAlchemy, and Alembic
+databases devops flask web-dev
+
+Using Flask-Login for User Management with Flask
+Using Flask-Login for User Management with Flask
+flask web-dev
+
+Jinja Templating Engine
+Primer on Jinja Templating
+basics flask web-dev
+
+Python Web Applications With Flask – Part III
+Python Web Applications With Flask – Part III
+flask web-dev
+
+Working with Django and Flask on Nitrous.IO
+Working with Django and Flask on Nitrous.IO
+django flask tools web-dev
+
+Kickstarting a Django Open Source Project
+Kickstarting a Django Open Source Project
+django web-dev
+
+Lyricize: A Flask app to create lyrics using Markov chains
+Lyricize: A Flask app to create lyrics using Markov chains
+flask projects web-dev
+
+Transaction Management with Django 1.6
+Transaction Management with Django 1.6
+advanced databases django web-dev
+
+Test driven development process
+Django 1.6 Test Driven Development
+basics django testing web-dev
+
+Rethink Flask – A Simple Todo List Powered by Flask and RethinkDB
+Rethink Flask – A Simple Todo List Powered by Flask and RethinkDB
+databases flask projects web-dev
+
+Python Web Applications With Flask – Part II
+Python Web Applications With Flask – Part II
+flask web-dev
+
+Migrating your Django Project to Heroku
+Migrating your Django Project to Heroku
+advanced databases devops django web-dev
+
+Generating Code Documentation with Pycco
+Generating Code Documentation with Pycco
+advanced django tools web-dev
+
+An Effective Sales Page with Bootstrap 3
+An Effective Sales Page with Bootstrap 3
+basics front-end web-dev
+
+Getting Started with Bootstrap 3
+Getting Started with Bootstrap 3
+basics front-end web-dev
+
+Testing in Django(Part 2) – Model Mommy vs Django Testing Fixtures
+Testing in Django(Part 2) – Model Mommy vs Django Testing Fixtures
+advanced django testing web-dev
+
+Python Web Applications with Flask – Part I
+Python Web Applications with Flask – Part I
+basics flask web-dev
+
+Testing in Django(Part 1) – Best Practices and Examples
+Testing in Django(Part 1) – Best Practices and Examples
+basics best-practices django testing web-dev
+
+Django tasty pie logo
+Create a Super Basic REST API with Django Tastypie
+api django projects web-dev
+
+Bottle logo
+Developing with Bottle – Part 2 (plot.ly API)
+data-science web-dev
+
+Modern Web Automation with Python and Selenium
+Headless Selenium Testing with Python and PhantomJS
+front-end intermediate testing web-dev web-scraping
+
+The web2py logo
+Web2py – Migrating from SQLite to MySQL
+advanced databases web-dev
